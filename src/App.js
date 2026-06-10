4ebca7083650b74b38c0f1d5b563927181103731
Reena Kanwar <reena041994@gmail.com>
2026-06-11 01:05:16 +0530

    fix bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   const [messages, setMessages] = useState([]);
-  const [inputFocused, setInputFocused] = useState(false);
+  const [ setInputFocused] = useState(false);
   const [inputText, setInputText] = useState('');
   
   // Custom router state
